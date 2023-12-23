61b0285659bafc1c6a43965fedf8e43015f1f47b
54m4n <pflum.tamas@gmail.com>
2023-12-23 12:16:39 +0100

    0

--- day04/day04-part2.py
+++ day04/day04-part2.py
@@ -18,29 +18,48 @@ for i in range(len(rows)):
         newrow.remove("")
     
     for j in range(len(newrow)):
-        if j<=6 and j>=2:
-        #if j<=1 and j>=2:
+        #if j<=6 and j>=2:
+        if j<=11 and j>=2:
             tempwin.append(newrow[j])
-        if j>=8:
-        #if j>=13:
+        #if j>=8:
+        if j>=13:
             tempown.append(newrow[j].replace("\n",""))
     win.append(tempwin)
     own.append(tempown)    
 
-print(win)
+print(win[0])
 print()
-print(own)
-
+print(own[0])
+print("--------")
 cardnums=[]
 wincardnums=[]
 matches=0
 
 for i in range(len(win)):
-    for j in range(len(win[i])):    
+    for j in range(len(win[i])):
         if win[i][j] in own[i]:
             matches=matches+1
-    cardnums.append(matches)
+    wincardnums.append(matches)
     matches=0
 
+print(wincardnums)
+cardnums=wincardnums.copy()
+
+try:
+    for i in range(len(wincardnums)):
+        mul=wincardnums[i]
+        for j in range(mul+1):
+            cardnums[i+j+1]=cardnums[i+j+1]*2
+except:
+    IndexError
+    
+print(wincardnums)
 print(cardnums)
 
+total=0
+for i in range(len(cardnums)):
+    total=total+cardnums[i]
+
+print(total+len(cardnums))
+
+#EZ EGY RAKAS SZAR!!!!!!!!!!!!
